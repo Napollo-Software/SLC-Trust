c2e9fe33dc51be348d1205b42833fafaa37ffd8d
alamgir000009 <alamgir.khan@napollo.online>
2024-10-03 18:05:30 +0500

    Update ForgotPassword.php

--- app/Mail/ForgotPassword.php
+++ app/Mail/ForgotPassword.php
@@ -11,26 +11,16 @@ class ForgotPassword extends Mailable
 {
     use Queueable, SerializesModels;
     public $details;
-     public $name;
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
+
     public function __construct($details,$name)
     {
         $this->details = $details;
         $this->name = $name;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
-        $app_name = config('app.name');
+        $app_name = config('app.professional_name');
         return $this->subject($app_name.' | Reset your password ')
         ->view('forgotPassword.forgotPassword');
     }
