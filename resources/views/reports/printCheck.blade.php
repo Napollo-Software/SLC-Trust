215cc09891e40dc105915fa6709c1699f8bae882
suleman-napollo <153205786+suleman-napollo@users.noreply.github.com>
2025-07-03 22:54:02 +0500

    pdf done checks

--- resources/views/reports/printCheck.blade.php
+++ resources/views/reports/printCheck.blade.php
@@ -13,6 +13,16 @@
         .no-print {
             display: none !important;
         }
+        @page {
+            margin: 0;
+            size: 8.5in 11in; /* Standard US letter size for consistent printing */
+        }
+      
+    }
+    html, body {
+      margin: 0;
+      padding: 0;
+      height: 100%;
     }
     .date {
         margin-right: 15%;
@@ -39,7 +49,6 @@
     }
     .number {
         font-family: "Courier New";
-
         text-align: right;
     }
     .date {
@@ -94,7 +103,6 @@
     .num:before {
         font-family: Helvetica;
         content: '$';
-
         position: absolute;
         left: -0.8em;
     }
@@ -145,240 +153,228 @@
         padding: 0 0 0 0.7em
     }
 .my-check {
-  position: relative;
-  width: 720px;              
-  height: 288px;             
+  position: relative;            
   background: #e3eef8;
   background-image: linear-gradient(to right, rgba(243, 246, 249, 0.15), rgba(125, 185, 232, 0.15)),url('https://subtlepatterns.com/patterns/connect.png');
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
   font-family: Helvetica, Arial, sans-serif;
   font-size: 14.4px;        
-  padding: 8px  4px;         
-}
-
-.my-check::before {
-  content: '';
-  position: absolute;
-  top: 7.2px;                
-  left: 7.2px;
-  width: calc(100% - 14.4px);
-  height: calc(100% - 14.4px);
-  border: 2px dotted #b2b6bc;
+  padding: 10px;    
+  margin: 20px 10px 10px 10px;    
+  border: 1px solid black;
+  border-style: dashed;   
 }
 
 /* Company & Address */
-.my-company {
-  position: absolute;
-  top: 14.4px;
-  left: 14.4px;
+.my-company { 
   font-weight: bold;
   line-height: 1.3;
 }
 
-.my-address {
-  position: absolute;
-  top: 37.44px;           
-  left: 14.4px;
+.my-address { 
   font-weight: normal;
   line-height: 1.3;
 }
 
 /* Check Number */
-.my-check-number {
-  position: absolute;
-  top: 14.4px;
-  right: 14.4px;
+.my-check-number { 
   font-family: "Courier New", monospace;
   font-size: 14.4px;
 }
 
 /* Date */
-.my-date {
-  position: absolute;
-  top: 70px;
-  right: 120px;           
+.my-date {  
   font-family: Damion, cursive;
-  font-size: 21.6px;         
-  width: 86.4px;            
-  text-align: right;
-  padding-right: 4.32px;   
-  border-bottom: 1px solid #666;
-}
-
-.my-date::before {
-  content: 'DATE';
-  position: absolute;
-  top: 25.92px;          
-  left: -40px;            
-  font-size: 10px;         
-  font-family: Helvetica, Arial, sans-serif;
+  font-size: 21.6px;     
 }
 
 /* Pay to the order of */
-.my-orderof {
-  position: absolute;
-  top: 120px;                
-  left: 70px;            
+.my-orderof {           
   font-family: Damion, cursive;
-  font-size: 25.92px;       
-  width: 60%;
-  padding-left: 11.52px;     
-  line-height: 1;
+  font-size: 25.92px;     
   border-bottom: 1px solid #666;
+  width : 98%; 
+  border-bottom: 1px solid black; 
+  padding-bottom: 2px;  
 }
 
-.my-orderof::before {
-  content: 'PAY TO THE ORDER OF';
-  position: absolute;
-  top: 25px;             
-  left: -50px;             
-  font-size: 10px;        
-  width: 55px;
-  font-family: Helvetica, Arial, sans-serif;
+.font-helvetica {
+   font-family: Helvetica, Arial, sans-serif;
+}
+.font-damion {
+   font-family: Damion, cursive;
 }
 
 /* Numeric Amount */
 .my-num {
-  position: absolute;
-  top: 120px;
-  right: 20px;
   font-family: Damion, cursive;
   font-size: 21.6px;
   border: 2px solid #666;
-  padding: 1px 8px;   
-  line-height: 1;
-}
-
-.my-num::before {
-  content: '$';
-  position: absolute;
-  top: 12px;               
-  left: -18px;            
-  font-family: Helvetica, Arial, sans-serif;
+  padding: 1px 4px;    
 }
 
 /* Amount in Words */
-.my-dollars {
-  position: absolute;
-  top: 170px;              
-  left: 14.4px;
+.my-dollars { 
   font-family: Damion, cursive;
-  font-size: 23.04px;        
+  font-size: 25.92px;       
   width: 70%;
   padding-left: 60px;    
   border-bottom: 1px solid #666;
 }
 
-.my-dollars::after {
-  content: 'DOLLARS';
-  position: absolute;
-  top: 25px;             
-  right: -75px;            
-  font-size: 14px;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
 /* Memo */
-.my-memo {
-  position: absolute;
-  bottom: 20px;            
-  left: 45px;              
+.my-memo {  
   font-family: Damion, cursive;
-  font-size: 20px;        
-  width: 55%;
+  font-size: 20px;    
   padding-left: 12px;    
   border-bottom: 1px solid #666;
 }
 
-.my-memo::before {
-  content: 'MEMO';
-  position: absolute;
-  padding-left: 12px;     
-  top: 15px;             
-  left: -50px;             
-  font-size: 10px;        
-  width: 55px;
-  font-family: Helvetica, Arial, sans-serif;
-}
-
 /* Signature */
-.my-signature {
-  position: absolute;
-  bottom: 36px;              
-  right: 14.4px;
+.my-signature { 
   font-family: 'Mrs Saint Delafield', cursive;
   font-size: 33.12px;     
-  padding-left: 7.2px;
-  line-height: 1;
-  border-bottom: 1px solid #666;
+  padding-left: 7.2px; 
 }
 
-
+ body {
+    background-image: url('assets/images/check-pdf-background-min.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    min-height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+ 
+ .w-50 {
+  width: 50%;
+ }
+ .w-full { 
+  width: 100%;
+ }
+.table-layout {
+  display: table;
+  width: 100%;
+}
+.table-row {
+  display: table-row;
+  width: 100%;
+}
+.table-cell {
+  display: table-cell; 
+}
+.text-end {
+  text-align: right;
+}
+ 
+ .page-break {
+      page-break-after: always
+  }
 </style>
 </head>
 <body>
     @foreach ($formDataArray as $formData)
-    <!-- <div class="check-container" style="margin-top:100px">
-        <div class="check print-check">
-            <div class="!row">
-                <div class="!col-md-6"><b>Trusted Surplus Solutions</b><br>59 Merrall Dr<br>Lawrence, NY 11559-1518</div>
-                <div class="!col-md-6 number check-number-text">{{ $formData['checkNumber'] ?? 'N/A' }}</div>
+    <div>
+        <div class="my-check"> 
+            <div class="table-layout">
+                <div class="table-row">
+                    <div class="table-cell w-50">
+                        <div class="my-company">
+                            <b>Trusted surplus Solutions</b>
+                        </div>
+                    </div>
+                    <div class="table-cell w-50 text-end">
+                        {{ $formData['checkNumber'] ?? 'N/A' }} 
+                    </div>
+                </div>
             </div>
-            <div class="date check-date-text">{{ $formData['checkDate'] ?? 'N/A' }}</div>
-            <div class="info">
-                <div class="orderof payee-text">{{ $formData['user'] ?? 'N/A' }}</div>
-                <div class="num amount-in-number-text">{{ $formData['checkNumber'] ?? 'N/A' }}</div>
-                <div class="dollars amount-in-word-text">{{ $formData['amountInWord'] ?? 'N/A' }}</div>
+        
+            <div class="my-address">
+                59 Merrall Dr 
             </div>
-            <div class="memo memo-text"><small>{{ $formData['memo'] ?? 'N/A' }}</small></div>
-            <div class="sig signature">{{ config('app.name') }}</div>
+            
+            <div class="table-layout">
+                <div class="table-row">
+                    <div class="table-cell w-50">
+                        Lawrence, NY 11559-1518 
+                    </div>
+                    <div class="table-cell w-50 text-end" style=" ">
+                        <span class="font-helvetica">Date:</span> <span class="my-date" style="border-bottom: 1px solid black; padding-bottom: 2px;">{{ $formData['checkDate'] ?? 'N/A' }}</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="table-layout" style="padding-top: 15px;">
+                <div class="table-row">
+                    <div class="table-cell" style="width: 90%">
+                        <div class="table-layout">
+                            <div class="table-row">
+                                <div class="table-cell" style="width: 7%; font-size: 10px;">
+                                    <div class="font-helvetica">PAY TO THE ORDER OF</div>
+                                </div>
+                                <div class="table-cell" style="width: 90%;">
+                                    <div class="font-damion my-orderof" style="padding-right: 5px;">{{ $formData['user'] ?? 'N/A' }}</div> 
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                    
+                    <div class="table-cell" style="width: 10%; vertical-align: bottom;">
+                        <div class="table-layout">
+                            <div class="table-row">
+                                <div class="table-cell" style="width: 10%; font-size: 18px; padding-right: 2px; vertical-align: bottom;">
+                                    $
+                                </div>
+                                <div class="table-cell" style="width: 90%;">
+                                    <div class="my-num"> 
+                                        {{ $formData['amountInWord'] ?? 'N/A' }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>  
+                    </div> 
+                </div>
+            </div>
+ 
+            <div class="table-layout">
+                <div class="table-row">
+                    <div class="table-cell my-dollars" style="width: 95%"> 
+                        {{ $formData['amountInNumber'] ?? 'N/A' }}  
+                    </div>
+                    <div class="table-cell font-helvetica" style="width: 5%; vertical-align: bottom;">
+                        DOLLARS
+                    </div> 
+                </div>
+            </div>
+ 
+            <div class="table-layout">
+                <div class="table-row">
+                    <div class="table-cell" style="width: 70%; vertical-align: bottom;"> 
+                        <div class="table-layout">
+                            <div class="table-row">
+                                <div class="table-cell" style="width: 5%; font-size: 10px; vertical-align: middle;">
+                                    <div class="font-helvetica">Memo</div>   
+                                </div> 
+                                <div class="table-cell" style="width: 92%;">
+                                    <div class="my-memo" style="padding-right: 5px;">{{ $formData['memo'] ?? 'N/A' }}</div> 
+                                </div>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="table-cell" style="width: 30%; vertical-align: middle;">
+                        <div class="my-signature">
+                            {{ config('app.name') }}
+                        </div>
+                    </div> 
+                </div>
+            </div>
         </div>
+        @if(!$loop->last)
+        <div class="page-break"></div>
+        @endif
+    </div>
 
-    </div> -->
-    <div class="my-check">
-  <div class="my-company">
-    <b>Trusted surplus Solutions</b>
-  </div>
-  <div class="my-address">
-    59 Merrall Dr<br>
-    Lawrence, NY 11559-1518
-  </div>
-  <div class="my-check-number">
-    <!-- 1025 -->
-    {{ $formData['checkNumber'] ?? 'N/A' }}
-</div>
-
-<div class="my-date">
-    <!-- 6/20/2023 -->
-    {{ $formData['checkDate'] ?? 'N/A' }}
-  </div>
-
-  <div class="my-orderof">
-    <!-- Customer -->
-    {{ $formData['user'] ?? 'N/A' }}
-  </div>
-  <div class="my-num">
-    <!-- 75.00  -->
-    {{ $formData['amountInWord'] ?? 'N/A' }}
-  </div>
-  <div class="my-dollars">
-    <!-- Seventy-five and 00/100 -->
-    {{ $formData['amountInNumber'] ?? 'N/A' }}
-
-  </div>
-
-  <div class="my-memo">
-    <small>
-      <!-- Aleksandra Gelman-Apt 9F, 2775 West 5ht streat (3B) Brooklyn, NY 11224 -->
-      {{ $formData['memo'] ?? 'N/A' }}
-    </small>
-  </div>
-
-  <div class="my-signature">
-    {{ config('app.name') }}
-  </div>
-</div>
     @endforeach
-
 </body>
 </html>
